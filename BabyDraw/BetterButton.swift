e4d93ad5bc3cbaaa1fe20af945e6075ba12d70d7
Robert Perez <Robert@roberts-mbp.simpsonmfg.com>
2017-03-09 12:53:44 -0800

    changed text scale factor for buttons for the tiny 4s

--- BabyDraw/BetterButton.swift
+++ BabyDraw/BetterButton.swift
@@ -17,7 +17,7 @@ class BetterButton: UIButton {
         
         
         self.titleLabel!.font = UIFont(name: "Marker Comp", size: 28)
-        self.titleLabel?.minimumScaleFactor = 0.5
+        self.titleLabel?.minimumScaleFactor = 0.4
         self.titleLabel?.numberOfLines = 0
         self.titleLabel?.adjustsFontSizeToFitWidth = true
         self.titleLabel?.lineBreakMode = NSLineBreakMode.byClipping
